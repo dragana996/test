301a144e38492ba62aae78d2d21d1cf7217ee72d
dragana996 <55789622+dragana996@users.noreply.github.com>
2020-03-05 09:53:15 +0100

    Instrumented Test for printing

--- app/src/androidTest/java/com/example/test/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/test/ExampleInstrumentedTest.java
@@ -1,22 +1,25 @@
-package com.example.test;
+package com.example.myapplication;
 
 import android.content.Context;
 
-import androidx.test.filters.LargeTest;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast;
 import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static java.util.regex.Pattern.matches;
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.*;
 
 /**
@@ -25,14 +28,43 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-@LargeTest
 public class ExampleInstrumentedTest {
 
     @Rule
-    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule(MainActivity.class);
+
     @Test
-    public void listGoesOverTheFold() {
-        onView(withText("Hellow Wold!")).check(matches(isEnabled()));
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("com.example.myapplication", appContext.getPackageName());
     }
 
+/*    @Test
+    public void check_Download() {
+        onView(withText("Download")).check(matches(isDisplayed()));
+        onView(withId(R.id.download_button)).check((matches(isEnabled())));
+        onView(withId(R.id.download_button)).perform(click());
+    }
+
+    @Test
+    public void check_Cancel() {
+        onView(withText("Cancel")).check((matches(isDisplayed())));
+        onView(withId(R.id.cancel_button)).check(matches(not(isEnabled())));
+    }
+
+    @Test
+    public void check_dis_Cancel() {
+        onView(withId(R.id.inf_textview)).check(matches(isDisplayed()));
+    }
+ */
+
+    @Test
+    public void printSmt() {
+        onView(withId(R.id.cancel_button)).check(matches(isDisplayed()));
+        onView(withId(R.id.cancel_button)).perform(click());
+
+        onView(withText("Success")).check(matches(isDisplayed()));
+    }
 }
